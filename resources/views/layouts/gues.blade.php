fe39c388a14229fea8f602662ac6aa2e8f82b9ee
musfiroh <dianmusfiroh90@gmail.com>
2022-08-20 11:44:38 +0800

    jquery add

--- resources/views/layouts/gues.blade.php
+++ resources/views/layouts/gues.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ secure_asset('css/gues.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/gues.css') }}">
     <link rel="shortcut icon" type="image/x-icon" href="https://image.ibb.co/fOur3b/favicon.png"/>
     <meta name="theme-color" content="#1885ed">
     <title>PMKS - Homepage</title>
@@ -469,6 +469,14 @@
   </footer>
 
   </body>
+  <!-- jquery -->
+  <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <!-- [/] jquery -->
+
+  <!-- bootstrap -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  <!-- [/] bootstrap -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
